cbcd9bb6379b7fd858ad9876fd0f7eeb0bdf76e6
Admin <shubhamraj8651@gmail.com>
2025-06-05 08:51:49 +0530

    Test2

---
+++ src/ex_01_Java_Basic/Java001_Java_Basic.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Java001_Java_Basic {
+    public static void main(String [] args){
+        System.out.println ("Hello world Git hub ");// sout
+        System.out.println("hrr");
+    }
+
+
+}
