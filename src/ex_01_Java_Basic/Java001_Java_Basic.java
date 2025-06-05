87c8a1f3dbd43f34b009d3d0321d03e04044673a
Admin <shubhamraj8651@gmail.com>
2025-06-05 09:02:02 +0530

    Test1

--- src/ex_01_Java_Basic/Java001_Java_Basic.java
+++ src/ex_01_Java_Basic/Java001_Java_Basic.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basic;
 public class Java001_Java_Basic {
     public static void main(String [] args){
         System.out.println ("Hello world Git hub ");// sout
-        System.out.println("hrr");
+        System.out.println("hrr87");
     }
 
 
