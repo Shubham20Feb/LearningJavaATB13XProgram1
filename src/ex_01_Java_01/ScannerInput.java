2c1920ea059d4d29e83327da1b5e8ef53e61bbf8
Shubham20Feb <shubhamraj8651@gmail.com>
2025-06-08 23:52:28 +0530

    Test1

---
+++ src/ex_01_Java_01/ScannerInput.java
@@ -0,0 +1,19 @@
+package ex_01_Java_01;
+
+import java.util.Scanner;
+
+public class ScannerInput {
+    public static void main (String[] args){
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("What is your age ");
+        int a = Sc.nextInt();
+        System.out.print("What is your name ");
+        Sc.nextLine();
+        String Name = Sc.nextLine();
+        System.out.print("Enater the float value");
+        float sd = Sc.nextFloat();
+        System.out.println("Hello " + Name + " Yor are " + a + " Year Old  " + "Float value  " + sd);
+
+
+    }
+}
